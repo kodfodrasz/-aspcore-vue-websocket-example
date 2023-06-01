81b3fb0fc46b31f5d684db61f53945b0a1f15a26
Gábor Gergely <ggergely@kodfodrasz.net>
2023-06-01 13:08:59 +0200

    HelloWorld component rewritten to Vue SFC style

--- vueapp/src/components/HelloWorld.vue
+++ vueapp/src/components/HelloWorld.vue
@@ -1,3 +1,36 @@
+<script setup lang="ts">
+    //import { defineComponent } from 'vue';
+    import { Ref, ref, onMounted, watch } from 'vue'
+
+    type Forecasts = {
+        date: string
+    }[];
+
+    const loading: Ref<boolean> = ref(false)
+    const post: Ref<null | Forecasts> = ref(null as null | Forecasts)
+
+    onMounted(async () => {
+        // fetch the data when the view is created and the data is
+        // already being observed
+        fetchData();
+    })
+
+    function fetchData() {
+        post.value = null;
+        loading.value = true;
+
+        fetch('weatherforecast')
+            .then(r => r.json())
+            .then(json => {
+                post.value = json as Forecasts;
+                loading.value = false;
+                return;
+            });
+    }
+
+    watch(() => '$route', fetchData)
+</script>
+
 <template>
     <div class="post">
         <div v-if="loading" class="loading">
@@ -26,48 +59,3 @@
         </div>
     </div>
 </template>
-
-<script lang="ts">
-    import { defineComponent } from 'vue';
-
-    type Forecasts = {
-        date: string
-    }[];
-
-    interface Data {
-        loading: boolean,
-        post: null | Forecasts
-    }
-
-    export default defineComponent({
-        data(): Data {
-            return {
-                loading: false,
-                post: null
-            };
-        },
-        created() {
-            // fetch the data when the view is created and the data is
-            // already being observed
-            this.fetchData();
-        },
-        watch: {
-            // call again the method if the route changes
-            '$route': 'fetchData'
-        },
-        methods: {
-            fetchData(): void {
-                this.post = null;
-                this.loading = true;
-
-                fetch('weatherforecast')
-                    .then(r => r.json())
-                    .then(json => {
-                        this.post = json as Forecasts;
-                        this.loading = false;
-                        return;
-                    });
-            }
-        },
-    });
-</script>
